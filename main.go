1c0811795d156cccbeabd66637451858cc65409b
zeelrupapara <zeelrupapara@gmail.com>
2022-10-12 16:49:22 +0530

    fix: change favicon

--- main.go
+++ main.go
@@ -17,6 +17,12 @@ func ConnectENV() {
 	fmt.Println("env loaded")
 }
 
+type TestJson struct {
+	Name   string `json:"name"`
+	Age    string `json:"age"`
+	Number string `json:"number"`
+}
+
 func main() {
 	ConnectENV()
 	app := fiber.New()
@@ -24,9 +30,16 @@ func main() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
+	app.Get("/api/v1/test", func(c *fiber.Ctx) error {
+		test := &TestJson{
+			Name:   "jeel",
+			Age:    "21",
+			Number: "0",
+		}
+		return c.JSON(test)
+	})
+	app.Static("/favicon.ico", "./public/image/favicon.ico")
 
-	app.Static("/favicon.ico", "./public/image/favicon.ico") 
-	
 	err := app.Listen(os.ExpandEnv(`:${PORT}`))
 	if err != nil {
 		fmt.Println(err)
